b49b3b42e1658a8f44910e9204bc683a0ba59e8e
w00j00ng <w00j00ng351@gmail.com>
2025-04-01 13:41:26 +0900

    fix: 주민등록번호 생성기 - 성별 수신

---
+++ jumin/README.md
@@ -0,0 +1,13 @@
+# Hello World
+
+## Install
+
+```sh
+$ go install github.com/w00j00ng/goproj/jumin@latest
+```
+
+## Run
+
+```sh
+$ jumin 1990-01-01
+```
--- jumin/main.go
+++ jumin/main.go
@@ -1,21 +1,26 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
-	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
-	// 인자가 없으면 사용법 출력
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: ./main.out YYYY-MM-DD")
+	// gender 플래그 정의 (기본값 "male")
+	gender := flag.String("gender", "male", "성별 지정: male 혹은 female")
+	flag.Parse()
+
+	// 남은 인자에서 생년월일을 가져옴
+	if flag.NArg() < 1 {
+		fmt.Println("Usage: ./main.out [--gender=male|female] YYYY-MM-DD")
 		return
 	}
 
-	inputDate := os.Args[1]
+	inputDate := flag.Arg(0)
 	// 날짜 파싱: layout은 "2006-01-02" 형식
 	t, err := time.Parse("2006-01-02", inputDate)
 	if err != nil {
@@ -27,30 +32,39 @@ func main() {
 	firstPart := t.Format("060102")
 
 	// 랜덤 시드 초기화
-	rand.Seed(time.Now().UnixNano())
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-	// 주민등록번호 뒷자리 7자리 중 첫번째 숫자는 생년월일에 따른 성별/세기 구분
-	var genderDigit int
-	if t.Year() < 2000 {
-		// 1900년대 출생인 경우: 1(남자) 또는 2(여자)
-		genderDigit = rand.Intn(2) + 1 // 1 또는 2
-	} else {
-		// 2000년대 출생인 경우: 3(남자) 또는 4(여자)
-		genderDigit = rand.Intn(2) + 3 // 3 또는 4
+	// 성별에 따른 주민등록번호 뒷자리 첫번째 숫자 결정
+	genderDigit := 1
+	if gender != nil {
+		switch strings.ToLower(*gender) {
+		case "male", "m", "man":
+			if t.Year() < 2000 {
+				genderDigit = 1 // 1900년대 남성
+			} else {
+				genderDigit = 3 // 2000년대 남성
+			}
+		case "female", "f", "woman":
+			if t.Year() < 2000 {
+				genderDigit = 2 // 1900년대 여성
+			} else {
+				genderDigit = 4 // 2000년대 여성
+			}
+		default:
+			genderDigit = 1 // 1900년대 남성
+		}
 	}
 
-	// 첫번째 뒷자리 생성 (성별/세기 구분)
+	// 뒷자리 첫번째 숫자 설정 후, 나머지 5자리 임의 숫자 생성
 	secondPartDigits := fmt.Sprintf("%d", genderDigit)
-	// 그 다음 5자리는 0~9 사이의 임의 숫자 생성
 	for i := 0; i < 5; i++ {
-		secondPartDigits += strconv.Itoa(rand.Intn(10))
+		secondPartDigits += strconv.Itoa(r.Intn(10))
 	}
 
-	// 12자리까지 결합 (앞 6자리 + 뒷자리 6자리)
+	// 앞 12자리 (6자리 생년월일 + 6자리 뒷자리) 생성
 	twelveDigits := firstPart + secondPartDigits
 
-	// 체크섯 계산: 각 자리수에 가중치 곱한 합계 구하기
-	// 가중치: [2,3,4,5,6,7,8,9,2,3,4,5]
+	// 체크섯 계산 (가중치: [2,3,4,5,6,7,8,9,2,3,4,5])
 	weights := []int{2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5}
 	sum := 0
 	for i, ch := range twelveDigits {
@@ -60,10 +74,8 @@ func main() {
 	remainder := sum % 11
 	checkDigit := (11 - remainder) % 10
 
-	// 최종 뒷자리 완성 (생성된 6자리 + 체크섯)
+	// 최종 주민등록번호 생성 ("YYMMDD-XXXXXXX")
 	fullSecondPart := secondPartDigits + strconv.Itoa(checkDigit)
-
-	// 주민등록번호: "YYMMDD-XXXXXXX" 형식
 	rrn := firstPart + "-" + fullSecondPart
 	fmt.Println(rrn)
 }
