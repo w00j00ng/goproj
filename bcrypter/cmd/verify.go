948d3791d5ceaa7a140facc0c92772e15fbed81c
w00j00ng <w00j00ng351@gmail.com>
2025-07-01 10:16:25 +0900

    fix: bcrypter verify args

--- bcrypter/cmd/verify.go
+++ bcrypter/cmd/verify.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+Copyright © 2025 NAME HERE w00j00ng351@gmail.com
 */
 package cmd
 
@@ -12,33 +12,33 @@ import (
 	"golang.org/x/term"
 )
 
+var (
+	hashArg     string
+	passwordArg string
+)
+
 // verifyCmd represents the verify command
 var verifyCmd = &cobra.Command{
 	Use:   "verify",
 	Short: "hash password verification",
-	Long: `./bcrypter verify <hash> <password>`,
+	Long: `./bcrypter verify --hash=<hash> --password=<password>`,
 	Run: func(cmd *cobra.Command, args []string) {
 		var hash string
 		var password string
 
-		if len(args) < 1 {
+		if hashArg == "" {
 			fmt.Print("Enter Hash: ")
 			hashBytes, _ := term.ReadPassword(int(syscall.Stdin))
 			hash = string(hashBytes)
 			fmt.Print("\r")
-		} else {
-			hash = args[0]
 		}
 
 		fmt.Print("\r")
 
-		if len(args) < 2 {
+		if passwordArg == "" {
 			fmt.Print("Enter Password: ")
 			passwordBytes, _ := term.ReadPassword(int(syscall.Stdin))
 			password = string(passwordBytes)
-		} else {
-			hash = args[0]
-			password = args[1]
 		}
 
 		err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
@@ -55,11 +55,6 @@ func init() {
 
 	// Here you will define your flags and configuration settings.
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// verifyCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// verifyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	verifyCmd.Flags().StringVar(&hashArg, "hash", "", "bcrypt hash to verify against")
+	verifyCmd.Flags().StringVarP(&passwordArg, "password", "p", "", "password to verify")
 }
